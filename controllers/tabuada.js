b18c511b6400245d6734c8a078ddad24c13b0989
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-24 01:49:45 -0300

    almost finished

--- controllers/tabuada.js
+++ controllers/tabuada.js
@@ -2,6 +2,16 @@ const list = (req,res) => {
     res.render('tabuada/list')
 }
 
+const tabuada = (req,res) => {
+    res.render('tabuada/tabuada', {
+        num: req.params.num
+    })
+}
+
+
+
+
 module.exports = {
-    list
+    list,
+    tabuada
 }
--- routes/tabuada.js
+++ routes/tabuada.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const tabuadaController = require('../controllers/tabuada')
 
-//o indexController.home é o ponto de ligação entre controller e main index.js
+//o tabuadaController.home é o ponto de ligação entre controller e main index.js
 router.get('/',tabuadaController.list )
+//traçar a rota da operação individual
+router.get('/do/:num', tabuadaController.tabuada)
 
 
 
